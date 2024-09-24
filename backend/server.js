9ab49aa130343fa06d35534fe782bc70341a94fa
ayushm98 <ayushkumarmalik10@gmail.com>
2024-09-24 18:36:46 -0400

    server updated

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,11 @@ app.use('/api/users', userRoutes);
 
 //app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
+
 /*
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
